80fc562007ac5b47bb100e82efb16910a6fdb394
kiracle-be <kiracle.le@metacrew.vn>
2025-09-09 15:20:17 +0700

    update export module nuxt3

--- src/module.ts
+++ src/module.ts
@@ -1,79 +1,95 @@
+import { defineNuxtModule } from "@nuxt/kit";
+import type { Module } from "@nuxt/types";
 
-import type { Module } from '@nuxt/types'
-import { resolveRuntimePath } from './utils'
-import { templates } from'./runtime/_templates'
-import { getOptions } from './options'
+import { resolveRuntimePath } from "./utils";
+import { templates } from "./runtime/_templates";
+import { getOptions } from "./options";
 
-export default <Module> function nuxt7 (options) {
+/* export default <Module> function nuxt7 (options) {
   this.nuxt.hook('build:before', () => {
     prepareBuild.call(this, options)
   })
-}
+} */
 
-function prepareBuild (_options) {
-  const options = getOptions.call(this, _options)
+export default defineNuxtModule({
+  meta: {
+    name: "nuxt7",
+    configKey: "nuxt7",
+  },
+
+  defaults: {},
+
+  setup(options, nuxt) {
+    nuxt.hooks.hook("build:before", () => {
+      prepareBuild(options);
+    });
+  },
+});
+
+function prepareBuild(_options) {
+  const options = getOptions.call(this, _options);
 
   // Force mode to SPA
-  this.options.mode = 'spa'
-  this.options.render.ssr = false
-  this.options.build.ssr = false
+  this.options.mode = "spa";
+  this.options.render.ssr = false;
+  this.options.build.ssr = false;
 
   // Extend webpack config
   this.extendBuild((config) => {
     // Increase performance check limits to 2M (non-gzipped)
-    const MAX_SIZE = 2 * 1024 * 1024
+    const MAX_SIZE = 2 * 1024 * 1024;
     Object.assign(config.performance, {
       maxEntrypointSize: MAX_SIZE,
-      maxAssetSize: MAX_SIZE
-    })
-  })
+      maxAssetSize: MAX_SIZE,
+    });
+  });
 
   // Transpile framework7
   this.options.build.transpile.push(
     /framework7[\\/](?!less)(?!components\/swiper\/swiper-src\/less)/,
-    'framework7-vue',
-    'template7',
-    'dom7'
-  )
+    "framework7-vue",
+    "template7",
+    "dom7"
+  );
 
   // Global theme color
   if (options.themeColor) {
     if (!this.options.manifest) {
-      this.options.manifest = {}
+      this.options.manifest = {};
     }
-    this.options.manifest.theme_color = options.themeColor
+    this.options.manifest.theme_color = options.themeColor;
   }
 
   // Webpack 4 cacheGroup
-  const { cacheGroups } = this.options.build.optimization.splitChunks
+  const { cacheGroups } = this.options.build.optimization.splitChunks;
   cacheGroups.framework7 = {
     test: /node_modules[\\/](framework7|framework7-vue|dom7|template7|ssr-window|path-to-regexp)[\\/]/,
-    chunks: 'all',
+    chunks: "all",
     priority: 10,
-    name: 'framework7'
-  }
+    name: "framework7",
+  };
 
   // Framework7 plugin
   this.addPlugin({
-    src: resolveRuntimePath('framework7/plugin.js'),
-    fileName: 'framework7/plugin.js',
-    options
-  })
+    src: resolveRuntimePath("framework7/plugin.js"),
+    fileName: "framework7/plugin.js",
+    options,
+  });
 
   // Copy all templates
   for (const template of templates) {
     this.addTemplate({
       src: resolveRuntimePath(template),
       fileName: template,
-      options
-    })
+      options,
+    });
   }
 
   // Enable nativeUI for a better Native experience
   if (!this.options.meta) {
-    this.options.meta = {}
+    this.options.meta = {};
   }
   if (this.options.meta.nativeUI === undefined) {
-    this.options.meta.nativeUI = true
+    this.options.meta.nativeUI = true;
   }
 }
